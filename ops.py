fd11225f1ebddce3f5c33b2a82c164c467819603
lovebear123 <cwtoronto90@gmail.com>
2019-05-23 21:53:23 -0700

    Update ops.py

--- ops.py
+++ ops.py
@@ -1,4 +1,8 @@
 """
+This file is taken without modification from https://github.com/hwalsuklee/tensorflow-generative-model-collections/
+"""
+
+"""
 Most codes from https://github.com/carpedm20/DCGAN-tensorflow
 """
 import math
